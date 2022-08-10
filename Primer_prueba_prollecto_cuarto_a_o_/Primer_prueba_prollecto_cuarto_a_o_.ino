448b45d423b32084b55a27f6619f6c4e0ae733a9
matikoblinc <78440014+matikoblinc@users.noreply.github.com>
2022-08-10 15:47:53 -0300

    10/8

--- Primer_prueba_prollecto_cuarto_a_o_/Primer_prueba_prollecto_cuarto_a_o_.ino
+++ Primer_prueba_prollecto_cuarto_a_o_/Primer_prueba_prollecto_cuarto_a_o_.ino
@@ -1,7 +1,8 @@
 #include <ESP8266WiFiMulti.h>
+#include <WiFiClient.h>
 
-
-  WiFiServer servidor (80);
+  WiFiServer servidor (80);}
+  char server[] = //Preguntarle a IVO que url poner acá o si deberíanmos comprar una. 
   
 void setup() {
   
@@ -14,7 +15,7 @@ void setup() {
   Serial.println ("Connecting");
 
   while (WiFi.status() != WL_CONNECTED)
-  {
+  { 
       delay (500);
       Serial.print (".");
   }
@@ -27,10 +28,51 @@ void setup() {
   servidor.begin(); 
 
    Serial.print (WiFi.localIP());
+
+  //client                          ver si uso cliente o servidor con IVOOOO
+
+    if (client.connect(server, 80)) {
+      // acá va el request de HTML para pedir los datos al servidor web 
+
+    client.println("GET /search?q=arduino HTTP/1.1");
+
+    client.println("Host: //nombre de la pagina");
+
+    client.println("Connection: close");
+
+    client.println();
+    }
 }
 
 void loop() {
 
+    listNetworks();
+
+ while (client.available()) {
+
+    char c = client.read();
+
+    Serial.write(c);
+
+  }
+
+  // if the server's disconnected, stop the client:
+
+  if (!client.connected()) {
+
+    Serial.println();
+
+    Serial.println("disconnecting from server.");
+
+    client.stop();
+
+     // do nothing forevermore:
+
+    while (true);
+
+  }
+}
+    
     WiFiClient client = servidor.available();
 
   if (client)
@@ -53,7 +95,6 @@ void loop() {
     client.flush(); 
   }
   
-prepareHtmlPage();
 
     client.println("HTTP/1.1 200 OK");
     client.println("Content-Type: text/html");
@@ -75,6 +116,30 @@ prepareHtmlPage();
     client.println("</html>");
 
 
+}
+
+void listNetworks() {
+  // scan for nearby networks:
+  Serial.println("** Scan Networks **");
+  int numSsid = WiFi.scanNetworks();
+  if (numSsid == -1) {
+    Serial.println("Couldn't get a wifi connection");
+    while (true);
+  }
+
+  // print the list of networks seen:
+  Serial.print("number of available networks:");
+  Serial.println(numSsid);
+  
+  String ArrayNetWorks[20];
+ 
+  // print the network number and name for each network found:
+  for (int thisNet = 0; thisNet < numSsid; thisNet++) {
+    Serial.print(thisNet);
+    Serial.print(") ");
+    Serial.print(WiFi.SSID(thisNet));
+    ArrayNetWorks[thisNet] = String(WiFi.SSID);
+  }
 }
 
 //String prepareHtmlPage()
